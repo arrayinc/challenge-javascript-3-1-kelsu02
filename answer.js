9f018e9e59714ce727dcef76898d0c1e3e8ccc33
kredmond <kelsu02@gmail.com>
2019-12-05 12:58:28 -0700

    fixed grammar

--- answer.js
+++ answer.js
@@ -3,15 +3,35 @@
 function beerCountDown(number) {
     let count = number;
     for (let i = number; i > 0; i--) {
-        console.log(`${count} bottles of beer on the wall.`);
-        console.log(`${count} bottles of beer.`);
-        count -= 1;
-        if (count > 0) {
+        if (count > 2) {
+            console.log(`${count} bottles of beer on the wall.`);
+            console.log(`${count} bottles of beer.`);
+            count -= 1;
             console.log(`You take one down, pass it around. ${count} bottles of beer on the wall.`);
-        } else {
-            console.log(`You take one down, pass it around. No bottles of beer on the wall.`);
+        }  else {
+            switch (count >= 1) {
+
+                case count === 2:
+                    console.log(`${count} bottles of beer on the wall.`);
+                    console.log(`${count} bottles of beer.`);
+                    count -= 1;
+                    console.log(`You take one down, pass it around. ${count} bottle of beer on the wall.`);
+                    break;
+
+                case count === 1:
+                    console.log(`${count} bottle of beer on the wall.`);
+                    console.log(`${count} bottle of beer.`);
+                    count -= 1;
+                    console.log(`You take one down, pass it around. ${count} bottles of beer on the wall.`);
+                    break;
+
+                default: 
+                    console.log("In heaven there is no beer");
+                    break;
+            }
         }
     }
+    console.log("You need to go to the store.");
 }
 
-beerCountDown(4);
+beerCountDown(6);
