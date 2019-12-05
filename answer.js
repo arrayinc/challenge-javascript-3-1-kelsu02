29a6109266e49a545529d2900625cf552ea46c4a
kredmond <kelsu02@gmail.com>
2019-12-05 11:54:49 -0700

    created count down with for loop.

--- answer.js
+++ answer.js
@@ -1,1 +1,17 @@
 // *** YOUR ANSWER BELOW ***
+
+function beerCountDown(number) {
+    let count = number;
+    for (let i = number; i > 0; i--) {
+        console.log(`${count} bottles of beer on the wall.`);
+        console.log(`${count} bottles of beer.`);
+        count -= 1;
+        if (count > 0) {
+            console.log(`You take one down, pass it around. ${count} bottles of beer on the wall.`);
+        } else {
+            console.log(`You take one down, pass it around. No bottles of beer on the wall.`);
+        }
+    }
+}
+
+beerCountDown(4);
